4fa48bda3ff7785cf80e816ad695d98eb80f4218
Kaleb Pierce <35708070+kpierc11@users.noreply.github.com>
2022-01-27 01:10:09 -0500

    theme changes

--- wordpress/wp-content/themes/customtheme/index.php
+++ wordpress/wp-content/themes/customtheme/index.php
@@ -12,8 +12,9 @@ if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post();
 
-		
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
 	}
+
 
 } else {
 
--- wordpress/wp-content/themes/customtheme/template-parts/header/site-nav.php
+++ wordpress/wp-content/themes/customtheme/template-parts/header/site-nav.php
@@ -5,28 +5,17 @@
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
+        <?php
+		wp_nav_menu(
+			array(
+				'theme_location'  => 'primary',
+				'menu_class'      => '',
+				'container_class' => 'collapse navbar-collapse',
+				'items_wrap'      => '<ul id="primary-menu-list" class="%2$s main-menu">%3$s</ul>',
+				'fallback_cb'     => false,
+			)
+		);
+		?>
       <form class="d-flex">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
